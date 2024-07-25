5bde686d66c1ef1e77a3519d6f71cc38029d688c
Alicoder001 <coderkuchkarov@gmail.com>
2024-07-25 11:03:11 +0500

    Home page UI

---
+++ app/(home)/components/index.ts
@@ -0,0 +1,7 @@
+import { Navbar } from "./navbar";
+import { Heroes } from "./heroes";
+import { Clients } from "./clients";
+import { Pricing } from "./pricing";
+import { PricingCard } from "./pricing-card";
+import { Footer } from "./footer";
+export { Navbar, Heroes, Clients, Pricing, PricingCard, Footer };
